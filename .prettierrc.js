6a783a25465aa9e4f581af515a68e3f0545a0566
Bruno Oliveira de Morais <exago.bomorais@universal.ad>
2023-04-19 12:19:10 -0300

    Criação do projeto, configuração eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true, //Adição de ; sempre que necessario
+    trailingComma: 'all', //Obrigatório a adiiconar virgula
+    singleQuote: true, //Aspas simples
+    printWidth: 80, //Tamanho maximo de cada linha
+    tabWidth: 2, // tamannho de espaços no tab
+};
